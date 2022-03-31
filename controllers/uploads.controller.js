5ccba5ada23daf8bdb71fba3771862c0c9f0c36f
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-03-31 17:19:56 -0500

    middleware para validar si viene un archivo

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,16 +1,10 @@
 const { response } = require("express");
-const { existeID, existeProductoById } = require("../helpers/db-validators");
 const { uploadFiles } = require("../helpers/upload-validator");
 const {User,Producto} =require("../models")
 
 const uploadFile=async(req,res=response) => {
 
-  if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
-    res.status(400).json({msg: 'No hay archivos para subir.'});
-    return;
-  }
-  const extensionesValidas=["png", "jpg", "jpeg", "gif","xlsx"];
-
+  const extensionesValidas=["png", "jpg", "jpeg", "gif"];
   try {
     // const pathFile= await uploadFiles(req.files,extensionesValidas,"kakaroto")
     const pathFile= await uploadFiles(req.files,undefined,"imgs")
@@ -23,11 +17,6 @@ const uploadFile=async(req,res=response) => {
 
 const updateImageDb=async (req, res) => {
 
-  if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
-    res.status(400).json({msg: 'No hay archivos para subir.'});
-    return;
-  }
-  
   const {collection,id}=req.params;
   let modelo;
   switch (collection) {
--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -3,13 +3,15 @@ const { check } = require('express-validator');
 const { uploadFile, updateImageDb } = require('../controllers/uploads.controller');
 const { coleecionesPermitidas } = require('../helpers/db-validators');
 const { validarCampos } = require('../middlewares/validar-campos');
+const { isFileUploaded } = require('../middlewares/validate-file');
 
 const router=Router();
 
 
-router.post('/', uploadFile);
+router.post('/',isFileUploaded, uploadFile);
 router.put('/:collection/:id',
   [
+    isFileUploaded,
     check('id','El id debe ser de mongo').isMongoId(),
     check('collection').custom(c => coleecionesPermitidas(c , ['users','productos'])),
     validarCampos
