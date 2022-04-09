bdeb6f9a84633179d40decaaeb5cf3ce1b6c0024
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-04-09 17:25:54 -0500

    BORRANDO IMAGENES EN CLOUDINARY

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -57,7 +57,10 @@ const updateImageDbCloudinary=async (req, res) => {
     // limpiar imagenes previas del usuario
     if(modelo.img){
       // hay que borrar la imagen del servidor
-
+      const nombreArr=modelo.img.split('/');
+      const nombre = nombreArr[nombreArr.length - 1];
+      const [public_id]=nombre.split('.');
+      cloudinary.uploader.destroy(public_id);
     }
     const {tempFilePath} = req.files.file;
 
